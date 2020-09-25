2d09697a88e5ed5534d34c8d1eb1bc80c8afd350
sckroll <kimsc0714@gmail.com>
2020-09-25 17:07:11 +0900

    Feat: 임시 랜딩 페이지 & 포스트 메인 페이지

--- pages/index.vue
+++ pages/index.vue
@@ -1,32 +1,15 @@
 <template>
-  <div>
-    <h1>블로그 포스트</h1>
-    <app-search-input />
-    <ul>
-      <li v-for="post of posts" :key="post.slug">
-        <nuxt-link :to="{ name: 'posts-slug', params: { slug: post.slug } }">
-          <img :src="post.img" />
-          <div>
-            <h2>{{ post.title }}</h2>
-            <p>{{ post.description }}</p>
-          </div>
-        </nuxt-link>
-      </li>
-    </ul>
+  <div class="container">
+    <div>홈페이지 리뉴얼 중... ㅜㅜ</div>
   </div>
 </template>
 
-<script>
-export default {
-  async asyncData({ $content, params }) {
-    const posts = await $content('posts', params.slug)
-      .only(['title', 'description', 'img', 'slug', 'tags'])
-      .sortBy('createdBy', 'asc')
-      .fetch()
-
-    return {
-      posts,
-    }
-  },
+<style>
+.container {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  text-align: center;
+  min-height: 100vh;
 }
-</script>
+</style>
---
+++ pages/posts/index.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <h1>블로그 포스트</h1>
+    <app-search-input />
+    <ul>
+      <li v-for="post of posts" :key="post.slug">
+        <nuxt-link :to="{ name: 'posts-slug', params: { slug: post.slug } }">
+          <img :src="post.img" />
+          <div>
+            <h2>{{ post.title }}</h2>
+            <p>{{ post.description }}</p>
+          </div>
+        </nuxt-link>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  async asyncData({ $content, params }) {
+    const posts = await $content('posts', params.slug)
+      .only(['title', 'description', 'img', 'slug', 'tags'])
+      .sortBy('createdBy', 'asc')
+      .fetch()
+
+    return {
+      posts,
+    }
+  },
+}
+</script>
