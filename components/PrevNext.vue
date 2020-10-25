351b9f11bdcdab23f083f3d2dca5cb653b788733
sckroll <kimsc0714@gmail.com>
2020-10-25 23:33:01 +0900

    Style: 링크에 마우스를 올렸을 때 효과 적용

--- components/PrevNext.vue
+++ components/PrevNext.vue
@@ -2,24 +2,22 @@
   <div class="prev-next-nav">
     <div class="prev-next-nav-wrapper">
       <span class="prev-post">
-        <span>이전 | </span>
+        <span class="prev-next-label">이전 |</span>
         <nuxt-link
           v-if="prev"
           :to="{ name: 'posts/slug', params: { slug: prev.slug } }"
+          >{{ prev.title }}</nuxt-link
         >
-          {{ prev.title }}
-        </nuxt-link>
         <span v-else class="post-empty">이전 포스트가 없습니다.</span>
       </span>
       <span class="next-post">
         <nuxt-link
           v-if="next"
           :to="{ name: 'posts/slug', params: { slug: next.slug } }"
+          >{{ next.title }}</nuxt-link
         >
-          {{ next.title }}
-        </nuxt-link>
         <span v-else class="post-empty">다음 포스트가 없습니다.</span>
-        <span> | 다음</span>
+        <span class="prev-next-label">| 다음</span>
       </span>
     </div>
   </div>
@@ -50,11 +48,36 @@ export default {
 .prev-next-nav-wrapper {
   width: var(--container-xl);
   padding: 15px 30px;
+  font-family: 'NanumSquare', sans-serif;
   font-size: 20px;
+}
+
+.prev-next-label {
+  font-weight: 700;
+}
+
+.prev-post a {
+  padding: 5px 5px;
+  transition: all 0.1s ease;
+}
+
+.prev-post a:hover {
+  background-color: #eeeeee;
+  transition: all 0.1s ease;
 }
 
 .next-post {
   float: right;
+}
+
+.next-post a {
+  padding: 5px 5px;
+  transition: all 0.1s ease;
+}
+
+.next-post a:hover {
+  background-color: #eeeeee;
+  transition: all 0.1s ease;
 }
 
 .post-empty {
