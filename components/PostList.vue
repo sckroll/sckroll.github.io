fdd5ecfea01c1a93e4b7e5d92a719cc07ae06410
sckroll <kimsc0714@gmail.com>
2020-10-26 17:22:02 +0900

    Refactor: 포스트 리스트 컴포넌트화

---
+++ components/PostList.vue
@@ -0,0 +1,71 @@
+<template>
+  <section class="content">
+    <div class="post-list-container">
+      <h2 class="component-label">블로그 포스트</h2>
+      <!-- <app-search-input /> -->
+      <section class="post-list">
+        <article v-for="post in posts" :key="post.slug" class="post-preview">
+          <post-preview :post="post" />
+        </article>
+      </section>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  props: {
+    posts: {
+      type: Array,
+      required: true,
+    },
+  },
+}
+</script>
+
+<style>
+.post-list-container {
+  width: var(--container-xl);
+  padding: 0 30px;
+}
+
+.post-list {
+  display: grid;
+  justify-content: space-between;
+  grid-template-columns: repeat(4, var(--post-preview-width-xl));
+  row-gap: 10px;
+}
+
+@media screen and (max-width: 1200px) {
+  .post-list-wrapper {
+    width: var(--container-lg);
+  }
+  .post-list {
+    grid-template-columns: repeat(3, var(--post-preview-width-lg));
+    row-gap: 13px;
+  }
+}
+
+@media screen and (max-width: 992px) {
+  .post-list-wrapper {
+    width: var(--container-md);
+  }
+  .post-list {
+    grid-template-columns: repeat(2, var(--post-preview-width-md));
+    row-gap: 14px;
+  }
+}
+
+@media screen and (max-width: 768px) {
+  .post-list-wrapper {
+    width: var(--container-sm);
+  }
+  .post-list {
+    grid-template-columns: repeat(1, 1fr);
+    row-gap: 10px;
+  }
+}
+
+@media screen and (max-width: 576px) {
+}
+</style>
--- pages/posts/index.vue
+++ pages/posts/index.vue
@@ -3,17 +3,7 @@
     <header class="page-header">
       <header-menu />
     </header>
-    <section class="content">
-      <div class="post-list-wrapper">
-        <h2 class="component-label">블로그 포스트</h2>
-        <!-- <app-search-input /> -->
-        <section class="post-list">
-          <article v-for="post in posts" :key="post.slug" class="post-preview">
-            <post-preview :post="post" />
-          </article>
-        </section>
-      </div>
-    </section>
+    <post-list :posts="posts" />
   </div>
 </template>
 
@@ -38,50 +28,5 @@ export default {
   justify-content: center;
   padding: 60px 0 20px 0;
   font-family: 'NanumSquare', sans-serif;
-}
-
-.post-list-wrapper {
-  width: var(--container-xl);
-  padding: 0 30px;
-}
-
-.post-list {
-  display: grid;
-  justify-content: space-between;
-  grid-template-columns: repeat(4, var(--post-preview-width-xl));
-  row-gap: 10px;
-}
-
-@media screen and (max-width: 1200px) {
-  .post-list-wrapper {
-    width: var(--container-lg);
-  }
-  .post-list {
-    grid-template-columns: repeat(3, var(--post-preview-width-lg));
-    row-gap: 13px;
-  }
-}
-
-@media screen and (max-width: 992px) {
-  .post-list-wrapper {
-    width: var(--container-md);
-  }
-  .post-list {
-    grid-template-columns: repeat(2, var(--post-preview-width-md));
-    row-gap: 14px;
-  }
-}
-
-@media screen and (max-width: 768px) {
-  .post-list-wrapper {
-    width: var(--container-sm);
-  }
-  .post-list {
-    grid-template-columns: repeat(1, 1fr);
-    row-gap: 10px;
-  }
-}
-
-@media screen and (max-width: 576px) {
 }
 </style>
