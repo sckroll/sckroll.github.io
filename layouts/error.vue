6f520ef2b91e69737714659816142aa7319e0177
sckroll <kimsc0714@gmail.com>
2021-01-02 18:32:23 +0900

    Feat: 에러 페이지 UI 변경

--- layouts/error.vue
+++ layouts/error.vue
@@ -37,6 +37,15 @@ export default {
 </script>
 
 <style>
+.error-container {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 100vh;
+}
+
 .error-code h1 {
   font-size: 64px;
   font-weight: 500;
