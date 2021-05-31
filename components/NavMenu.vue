82287d7a2a9da744b88fef768eb8c352e73a4523
sckroll <kimsc0714@gmail.com>
2021-05-31 23:39:06 +0900

    Style: SCSS 중첩 문법 적용

--- components/NavMenu.vue
+++ components/NavMenu.vue
@@ -23,36 +23,35 @@
   padding: 0;
   font-size: 24px;
   font-weight: 500;
+
+  ul {
+    padding: 0;
+  }
+  li {
+    margin: 0 10px;
+    display: inline;
+    transition: all 0.2s ease;
+
+    &:first-child {
+      margin-left: 0;
+    }
+    &:last-child {
+      margin-right: 0;
+    }
+    &:hover {
+      color: #bbbbbb;
+      transition: all 0.2s ease;
+    }
+  }
 }
 
-.nav-menu ul {
-  padding: 0;
-}
+.landing-header {
+  .nav-menu {
+    color: #ffffff88;
 
-.nav-menu li {
-  margin: 0 10px;
-  display: inline;
-  transition: all 0.2s ease;
-}
-
-.nav-menu li:first-child {
-  margin-left: 0;
-}
-
-.nav-menu li:last-child {
-  margin-right: 0;
-}
-
-.nav-menu li:hover {
-  color: #bbbbbb;
-  transition: all 0.2s ease;
-}
-
-.landing-header .nav-menu {
-  color: #ffffff88;
-}
-
-.landing-header .nav-menu li:hover {
-  color: #ffffff;
+    li:hover {
+      color: #ffffff;
+    }
+  }
 }
 </style>
