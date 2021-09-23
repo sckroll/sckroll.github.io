7ea98e09c9655efddc196928446adde68311e834
sckroll <kimsc0714@gmail.com>
2021-09-24 02:17:19 +0900

    style: 컬러 코드를 색상 이름으로 변경

--- nuxt.config.js
+++ nuxt.config.js
@@ -9,7 +9,7 @@ export default {
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: '' },
-      { name: 'theme-color', content: '#ffffff' },
+      { name: 'theme-color', content: 'white' },
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
