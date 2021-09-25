3b5d5de74063f0f353225cbdc8b35f876e33ed0b
sckroll <kimsc0714@gmail.com>
2021-09-25 17:07:27 +0900

    style: 주석 추가

--- utils/metaThemeColor.js
+++ utils/metaThemeColor.js
@@ -1,9 +1,10 @@
 export const setMetaThemeColor = () => {
-  // 모바일 브라우저 상태바 색상 변경
+  // 색상 변수 추출
   const metaThemeColor = getComputedStyle(
     document.documentElement,
   ).getPropertyValue('--color-meta-theme')
 
+  // 모바일 브라우저 상태 바에 색상 적용
   document
     .querySelector('meta[name="theme-color"]')
     .setAttribute('content', metaThemeColor)
