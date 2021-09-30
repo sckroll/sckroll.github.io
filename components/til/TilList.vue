439841e54cbab1ea5329fc36da6d494e98e616a2
sckroll <kimsc0714@gmail.com>
2021-09-30 22:10:34 +0900

    feat(style): TIL 페이지를 리스트 영역과 내용 영역으로 분리

---
+++ components/til/TilList.vue
@@ -0,0 +1,35 @@
+<template>
+  <section>
+    <div>TIL LIST</div>
+  </section>
+</template>
+
+<script>
+export default {
+  props: {
+    items: {
+      type: Array,
+      required: true,
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+@include set-viewpoint;
+
+section {
+  flex: 1;
+}
+
+@include viewpoint-xl {
+}
+@include viewpoint-lg {
+}
+@include viewpoint-md {
+}
+@include viewpoint-sm {
+}
+@include viewpoint-xs {
+}
+</style>
--- pages/til/index.vue
+++ pages/til/index.vue
@@ -1,18 +1,70 @@
 <template>
   <div class="til-page">
-    <h2 class="under-construction">준비 중 입니다.</h2>
+    <h1>Today I Learned</h1>
+    <div class="til-content-area">
+      <til-list :items="items"></til-list>
+      <til-content :items="items"></til-content>
+    </div>
   </div>
 </template>
+
+<script>
+export default {
+  async asyncData({ $content, error }) {
+    try {
+      const items = await $content('til', { deep: true }).fetch()
+
+      return {
+        items,
+      }
+    } catch (e) {
+      error({ statusCode: e.statusCode || e.status || 500 })
+    }
+  },
+}
+</script>
 
 <style lang="scss">
 @include set-viewpoint;
 
 .til-page {
-  width: 100%;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
   margin-top: $header-height;
-  padding: 32px 0;
+  padding: 64px 32px;
+
+  h1 {
+    font-size: 1.5em;
+    margin-bottom: 32px;
+  }
+}
+.til-content-area {
+  display: flex;
+  align-items: center;
+  gap: 32px;
+}
+
+@include viewpoint-xl {
+  .til-page {
+    width: $breakpoint-xl;
+  }
+}
+@include viewpoint-lg {
+  .til-page {
+    width: $breakpoint-lg;
+  }
+}
+@include viewpoint-md {
+  .til-page {
+    width: $breakpoint-md;
+  }
+}
+@include viewpoint-sm {
+  .til-page {
+    width: $breakpoint-sm;
+  }
+}
+@include viewpoint-xs {
+  .til-page {
+    width: 100%;
+  }
 }
 </style>
