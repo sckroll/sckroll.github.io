fa7c00f85a191e0f7ee7fdf0190b54b1c4b0a2b1
김성찬 <kimsc0714@naver.com>
2022-03-09 03:10:50 +0900

    refactor: 로고 컨테이너 컴포넌트 추가

---
+++ components/future/common/Logo.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="logo-container">
+    <SvgBase :width="102" :height="24">
+      <LogoText></LogoText>
+    </SvgBase>
+    <SvgBase>
+      <IconLogo></IconLogo>
+    </SvgBase>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss" scoped>
+.logo-container {
+  display: flex;
+  gap: 24px;
+  align-items: center;
+}
+</style>
