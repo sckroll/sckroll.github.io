931af62640d5cdc4c8691cdca57bf09a86932300
김성찬 <kimsc0714@naver.com>
2022-03-09 02:58:45 +0900

    refactor: 아이콘을 표현할 수 있는 기반 컴포넌트 생성

---
+++ components/future/common/icon/IconBase.vue
@@ -0,0 +1,36 @@
+<template>
+  <svg
+    :width="width"
+    :height="height"
+    viewBox="0 0 32 32"
+    :fill="fill"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <slot></slot>
+  </svg>
+</template>
+
+<script>
+export default {
+  props: {
+    fill: {
+      type: String,
+      default: 'black',
+    },
+    width: {
+      type: Number,
+      default: 32,
+    },
+    height: {
+      type: Number,
+      default: 32,
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.dark-mode svg {
+  fill: white;
+}
+</style>
