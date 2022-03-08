5439d6811ba14d3d3becad7b2d436ee550c1ca3d
김성찬 <kimsc0714@naver.com>
2022-03-09 03:58:49 +0900

    feat(style): 헤더 메뉴 구현

--- components/future/common/NewHeaderBase.vue
+++ components/future/common/NewHeaderBase.vue
@@ -1,12 +1,35 @@
 <template>
-  <header></header>
+  <header>
+    <div class="header-container">
+      <div class="header-center">
+        <Logo></Logo>
+        <NewHeaderMenu></NewHeaderMenu>
+      </div>
+    </div>
+  </header>
 </template>
 
 <script>
 export default {}
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
 header {
+  display: flex;
+  justify-content: center;
+
+  .header-container {
+    width: 1200px;
+    height: 96px;
+    display: flex;
+    align-items: flex-end;
+    padding: 0 32px 8px;
+  }
+  .header-center {
+    width: 100%;
+    height: 48px;
+    display: flex;
+    justify-content: space-between;
+  }
 }
 </style>
---
+++ components/future/common/NewHeaderMenu.vue
@@ -0,0 +1,36 @@
+<template>
+  <nav class="header-menu">
+    <ul>
+      <li>About</li>
+      <li>Posts</li>
+      <li>Projects</li>
+      <li>
+        <SvgBase icon :width="24" :height="24">
+          <IconLogo></IconLogo>
+        </SvgBase>
+      </li>
+    </ul>
+  </nav>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss" scoped>
+nav.header-menu {
+  display: flex;
+  align-items: center;
+}
+ul {
+  list-style: none;
+  display: flex;
+  justify-content: space-between;
+  gap: 32px;
+  font-family: 'Metropolis', sans-serif;
+  font-size: 1.25em;
+}
+li {
+  height: 1.25em;
+}
+</style>
