0fed3a99e3158240ba9a6d0d21ebf2854395a479
김성찬 <kimsc0714@naver.com>
2022-03-10 23:52:11 +0900

    refactor: 헤더 메시지 코드 위치 이동

--- pages/components.vue
+++ pages/components.vue
@@ -1,5 +1,16 @@
 <template>
-  <div class="new-page-components">test</div>
+  <div class="new-page-container">
+    <section class="header-area">
+      <div class="header-area-center">
+        <h1>
+          프론트엔드 개발자가 되기 위해<br />
+          오늘도 공부하는 Sckroll의<br />
+          홈페이지에 오신 것을 환영합니다.
+        </h1>
+      </div>
+    </section>
+    <section class="content-area"></section>
+  </div>
 </template>
 
 <script>
@@ -9,8 +20,21 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-.new-page-components {
+.new-page-container {
   width: 1200px;
-  padding: 64px 32px;
+  padding: 0 32px;
+}
+section.header-area {
+  height: calc(70vh - $header-height);
+  display: flex;
+  align-items: center;
+}
+.header-area-center {
+  h1 {
+    font-size: 2em;
+    font-weight: 300;
+  }
+}
+section.content-area {
 }
 </style>
