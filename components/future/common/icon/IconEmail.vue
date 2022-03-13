61d663331fd1ec1eb887e0c99caf7391a1137fcf
김성찬 <kimsc0714@naver.com>
2022-03-13 22:49:10 +0900

    feat(style): 이메일 아이콘 컴포넌트 추가

---
+++ components/future/common/icon/IconEmail.vue
@@ -0,0 +1,7 @@
+<template>
+  <path
+    fill-rule="evenodd"
+    clip-rule="evenodd"
+    d="M2 4H30V28H2V4ZM4 6V9.4412L16 16.8258L28 9.4412V6H4ZM28 11.7896L16 19.1742L4 11.7896V26H28V11.7896Z"
+  />
+</template>
