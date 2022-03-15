7a3b7eb43be4672e377349003d6ccd09ee80a89b
김성찬 <kimsc0714@naver.com>
2022-03-15 23:54:49 +0900

    feat(style): 토글 스위치 컴포넌트 생성

---
+++ components/future/common/ToggleSwitch.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="toggle-switch">
+    <div class="knob"></div>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss" scoped>
+.toggle-switch {
+  width: 48px;
+  height: 24px;
+  display: flex;
+  align-items: center;
+  border: 2px solid white;
+  border-radius: 12px;
+  padding: 2px;
+}
+.knob {
+  width: 16px;
+  height: 16px;
+  background-color: white;
+  border-radius: 8px;
+}
+</style>
