953d8de832aab3a14ea2b09e4afbe59003aed6f3
김성찬 <kimsc0714@naver.com>
2022-03-16 02:23:19 +0900

    feat(style): 클릭 시 노브가 움직이는 모션 추가

--- components/future/common/ToggleSwitch.vue
+++ components/future/common/ToggleSwitch.vue
@@ -1,15 +1,28 @@
 <template>
-  <div class="toggle-switch">
-    <div class="knob"></div>
+  <div class="toggle-switch" :class="{ toggled: value }" @click="toggle">
+    <div class="knob" :class="{ toggled: value }"></div>
   </div>
 </template>
 
 <script>
-export default {}
+export default {
+  props: {
+    value: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  methods: {
+    toggle() {
+      this.$emit('input', !this.value)
+    },
+  },
+}
 </script>
 
 <style lang="scss" scoped>
 .toggle-switch {
+  cursor: pointer;
   width: 48px;
   height: 24px;
   display: flex;
@@ -17,11 +30,21 @@ export default {}
   border: 2px solid white;
   border-radius: 12px;
   padding: 2px;
+  transition: background-color 0.15s ease;
+
+  &.toggled {
+    background-color: $color-primary;
+  }
 }
 .knob {
   width: 16px;
   height: 16px;
   background-color: white;
   border-radius: 8px;
+  transition: transform 0.15s ease;
+
+  &.toggled {
+    transform: translateX(150%);
+  }
 }
 </style>
