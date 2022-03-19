dbf99ed4fb69e619cdbc34518147c0b2bead28d7
김성찬 <kimsc0714@naver.com>
2022-03-20 00:45:22 +0900

    feat(style): 메인 화면 상단 소개 문구 추가

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,24 @@
 <template>
-  <div class="landing-container">
+  <!-- <div class="landing-container">
     <post-list featured :posts="posts">
       <template v-slot:title>최근 포스트</template>
     </post-list>
+  </div> -->
+  <div class="page-container">
+    <section class="header-area">
+      <div class="header-area-center">
+        <h1>
+          프론트엔드 개발자가 되기 위해<br />
+          오늘도 공부하는 Sckroll의<br />
+          홈페이지에 오신 것을 환영합니다.
+        </h1>
+        <nuxt-link to="/about">제가 누군지 궁금하세요?</nuxt-link>
+      </div>
+    </section>
+    <section class="content-area">
+      <h1>컴포넌트 테스트</h1>
+      <ToggleSwitch v-model="toggle"></ToggleSwitch>
+    </section>
   </div>
 </template>
 
@@ -31,6 +47,11 @@ export default {
       error({ statusCode: e.statusCode || e.status || 500 })
     }
   },
+  data() {
+    return {
+      toggle: false,
+    }
+  },
   head() {
     return {
       title: '홈',
@@ -40,7 +61,60 @@ export default {
 </script>
 
 <style lang="scss">
-.landing-container {
-  margin-top: $header-height;
+.page-container {
+  padding: 0 32px;
+}
+section.header-area {
+  height: calc(70vh - $header-height);
+  display: flex;
+  align-items: center;
+}
+.header-area-center {
+  h1 {
+    font-size: 2em;
+    font-weight: 300;
+    margin-bottom: 32px;
+    line-height: 1.5em;
+  }
+  a {
+    font-size: 1.25em;
+    padding-bottom: 4px;
+    border-bottom: 2px solid white;
+    transition: all 0.15s ease;
+
+    &:hover {
+      color: $color-primary;
+      border-bottom: 2px solid $color-primary;
+    }
+  }
+}
+section.content-area {
+  margin: 32px 0;
+}
+
+@include viewpoint-xl {
+  .page-container {
+    width: $breakpoint-xl;
+  }
+}
+@include viewpoint-lg {
+  .page-container {
+    width: $breakpoint-lg;
+  }
+}
+@include viewpoint-md {
+  .page-container {
+    width: $breakpoint-md;
+  }
+}
+@include viewpoint-sm {
+  .page-container {
+    width: $breakpoint-sm;
+  }
+}
+@include viewpoint-xs {
+  .page-container {
+    width: 100%;
+  }
 }
 </style>
