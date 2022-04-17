c441d343f377d4c9be80cd4546fd7646c321453d
김성찬 <kimsc0714@naver.com>
2022-04-17 18:15:06 +0900

    feat(style): 스크롤 시 로고 아이콘만 남도록 애니메이션 적용

--- components/common/Logo.vue
+++ components/common/Logo.vue
@@ -1,18 +1,27 @@
 <template>
-  <nuxt-link
-    to="/"
-    class="logo-container"
-    :class="{ 'fix-light': hasHeaderImage, scrolled: isScrolled }"
-  >
-    <transition name="fade">
-      <SvgBase v-if="!isScrolled" width="102" height="24">
+  <div class="logo-container">
+    <div
+      class="logo-area"
+      :class="{ 'fix-light': hasHeaderImage, scrolled: isScrolled }"
+    >
+      <SvgBase
+        class="logo-text"
+        :class="{ scrolled: isScrolled }"
+        width="102"
+        height="24"
+      >
         <LogoText></LogoText>
       </SvgBase>
-    </transition>
-    <SvgBase class="logo-icon">
-      <IconLogo></IconLogo>
-    </SvgBase>
-  </nuxt-link>
+      <SvgBase class="logo-icon">
+        <IconLogo></IconLogo>
+      </SvgBase>
+    </div>
+    <nuxt-link
+      to="/"
+      class="logo-link"
+      :class="{ scrolled: isScrolled }"
+    ></nuxt-link>
+  </div>
 </template>
 
 <script>
@@ -31,20 +40,48 @@ export default {
 <style lang="scss" scoped>
 .logo-container {
   display: flex;
-  gap: 24px;
-  align-items: center;
+}
+.logo-link {
+  position: absolute;
+  width: 158px;
+  height: 32px;
   border-bottom: none;
-  transition: $fade-default;
+  padding: 0;
 
   &:hover,
   &:active {
     border-bottom: none;
   }
+  &.scrolled {
+    width: 32px;
+  }
+}
+.logo-area {
+  display: flex;
+  gap: 24px;
+  align-items: center;
+  overflow: hidden;
+  transition: transform 0.25s cubic-bezier(0, 0.7, 0, 1);
+
   &.fix-light svg {
     fill: white;
   }
-  &.scrolled svg {
-    fill: white;
+  &.scrolled {
+    transform: translateX(-126px);
+
+    svg {
+      fill: white;
+    }
+  }
+
+  .logo-text {
+    opacity: 1;
+    transition: opacity 0.2s cubic-bezier(0, 0.7, 0, 1);
+
+    &.scrolled {
+      opacity: 0;
+      transition: opacity 0.2s cubic-bezier(0, 0.7, 0, 1);
+    }
   }
 }
 </style>
