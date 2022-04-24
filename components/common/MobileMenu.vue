89cfaf6d055c32ee1c8a535cf22fcffda9ad0ae6
김성찬 <kimsc0714@naver.com>
2022-04-24 15:29:11 +0900

    feat(style): 모바일 메뉴 디자인 변경

--- components/common/MobileMenu.vue
+++ components/common/MobileMenu.vue
@@ -1,16 +1,15 @@
 <template>
-  <nav class="drawer">
-    <ul>
-      <li v-for="item in menu" :key="item.name">
-        <nuxt-link :to="item.path" @click.native="closeMenu">
-          {{ item.name }}
-        </nuxt-link>
-      </li>
-    </ul>
-    <div class="toggle-container">
-      <ColorModeToggle drawer></ColorModeToggle>
-    </div>
-  </nav>
+  <div class="mobile-menu-container">
+    <nav class="mobile-menu-nav">
+      <ul>
+        <li v-for="item in menu" :key="item.name">
+          <nuxt-link :to="item.path" @click.native="closeMenu">
+            {{ item.name }}
+          </nuxt-link>
+        </li>
+      </ul>
+    </nav>
+  </div>
 </template>
 
 <script>
@@ -36,7 +35,7 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-nav.drawer {
+.mobile-menu-container {
   position: absolute;
   top: 0;
   right: 0;
@@ -48,66 +47,62 @@ nav.drawer {
   box-shadow: 0 2px 4px 2px rgba(black, 0.2);
   width: 100%;
   height: 100vh;
-  padding: 48px 32px;
+  padding: calc($header-height + 16px) 32px;
   transition: $fade-default;
+}
+ul {
+  list-style: none;
+  display: flex;
+  flex-direction: column;
+  gap: 32px;
+  font-family: Metropolis, sans-serif;
+  font-size: 2.5em;
+  padding: 0;
+}
+li {
+  a {
+    border-bottom: 4px solid transparent;
+    transition: $fade-default;
 
-  ul {
-    list-style: none;
-    display: flex;
-    flex-direction: column;
-    gap: 16px;
-    font-family: Metropolis, sans-serif;
-    font-size: 1.5em;
-    padding: 0;
-  }
-  li {
-    a {
-      padding: 4px 0;
-      border-top: 3px solid transparent;
-      border-bottom: 3px solid transparent;
-      transition: $fade-default;
-
-      &:hover {
-        border-bottom: 3px solid $color-primary;
-        transition: $fade-default;
-      }
-      &:active {
-        color: $color-primary;
-        transition: $fade-default;
-      }
+    &:hover {
+      border-bottom: 4px solid $color-primary;
     }
   }
 }
-.toggle-container {
-  display: flex;
-}
-.dark-mode nav.drawer {
-  background-color: rgba(black, 0.7);
-  color: white;
+.dark-mode {
+  .mobile-menu-container {
+    background-color: rgba(black, 0.7);
+    color: white;
+  }
+  a {
+    &:hover {
+      border-bottom: 4px solid $color-secondary;
+    }
+  }
 }
 
 @include viewpoint-xl {
-  nav.drawer {
+  .mobile-menu-container {
     display: none;
   }
 }
 @include viewpoint-lg {
-  nav.drawer {
+  .mobile-menu-container {
     display: none;
   }
 }
 @include viewpoint-md {
-  nav.drawer {
+  .mobile-menu-container {
     display: none;
   }
 }
 @include viewpoint-sm {
-  nav.drawer {
+  .mobile-menu-container {
     display: flex;
   }
 }
 @include viewpoint-xs {
-  nav.drawer {
+  .mobile-menu-container {
     display: flex;
   }
 }
