acf5d78343a065f26aa32f72e96b3c6eebc5513f
김성찬 <kimsc0714@naver.com>
2022-04-27 23:53:21 +0900

    refactor: 랜딩 페이지 상단 메시지를 컴포넌트로 분리

---
+++ components/common/HeaderIntroMessage.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="header-intro-message">
+    <h1>
+      프론트엔드 개발자가 되기 위해<br />
+      오늘도 공부하는 Sckroll의<br />
+      홈페이지에 오신 것을 환영합니다.
+    </h1>
+    <nuxt-link to="/about">제가 누군지 궁금하세요?</nuxt-link>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss" scoped>
+.header-intro-message {
+  h1 {
+    font-size: 2em;
+    font-weight: 300;
+    margin-bottom: 32px;
+    line-height: 1.5em;
+  }
+  a {
+    font-size: 1.25em;
+    padding-bottom: 4px;
+    border-bottom: 2px solid white;
+
+    &:hover {
+      border-bottom: 2px solid $color-secondary;
+    }
+    &:active {
+      color: $color-secondary;
+    }
+  }
+}
+
+@include viewpoint-xl {
+  .page-container {
+    width: $breakpoint-xl;
+  }
+}
+@include viewpoint-lg {
+  .page-container {
+    width: $breakpoint-lg;
+  }
+}
+@include viewpoint-md {
+  .page-container {
+    width: $breakpoint-md;
+  }
+}
+@include viewpoint-sm {
+  .page-container {
+    width: $breakpoint-sm;
+  }
+}
+@include viewpoint-xs {
+  .page-container {
+    width: 100%;
+  }
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,7 @@
 <template>
   <div class="page-container">
     <section class="header-area">
-      <div class="header-area-center">
-        <h1>
-          프론트엔드 개발자가 되기 위해<br />
-          오늘도 공부하는 Sckroll의<br />
-          홈페이지에 오신 것을 환영합니다.
-        </h1>
-        <nuxt-link to="/about">제가 누군지 궁금하세요?</nuxt-link>
-      </div>
+      <HeaderIntroMessage></HeaderIntroMessage>
     </section>
     <section class="content-area">
       <NewPostListContents :posts="posts"></NewPostListContents>
@@ -44,31 +37,6 @@ export default {
       error({ statusCode: e.statusCode || e.status || 500 })
     }
   },
-  data() {
-    return {
-      toggle: false,
-      dummyPosts: [
-        {
-          slug: 'dummy',
-          title:
-            '더미 포스트 더미 포스트 더미 포스트 더미 포스트 더미 포스트 더미 포스트 더미 포스트 더미 포스트 더미 포스트 더미 포스트 더미 포스트',
-          description:
-            '포스트 아이템 컴포넌트 테스트용 포스트입니다. 포스트 아이템 컴포넌트 테스트용 포스트입니다.포스트 아이템 컴포넌트 테스트용 포스트입니다.포스트 아이템 컴포넌트 테스트용 포스트입니다.포스트 아이템 컴포넌트 테스트용 포스트입니다.포스트 아이템 컴포넌트 테스트용 포스트입니다.포스트 아이템 컴포넌트 테스트용 포스트입니다.포스트 아이템 컴포넌트 테스트용 포스트입니다.포스트 아이템 컴포넌트 테스트용 포스트입니다.포스트 아이템 컴포넌트 테스트용 포스트입니다.포스트 아이템 컴포넌트 테스트용 포스트입니다.포스트 아이템 컴포넌트 테스트용 포스트입니다.포스트 아이템 컴포넌트 테스트용 포스트입니다.포스트 아이템 컴포넌트 테스트용 포스트입니다.',
-          tags: 'test, asdf, aaaa',
-          createdAt: new Date(),
-          updatedAt: new Date('2022-04-10'),
-        },
-        {
-          slug: 'dummy2',
-          title: '더미 포스트',
-          description: '포스트 아이템 컴포넌트 테스트용 포스트입니다.',
-          tags: 'test',
-          createdAt: new Date(),
-          updatedAt: new Date('2022-04-10'),
-        },
-      ],
-    }
-  },
   head() {
     return {
       title: '홈',
@@ -86,26 +54,6 @@ section.header-area {
   display: flex;
   align-items: center;
   color: white;
-}
-.header-area-center {
-  h1 {
-    font-size: 2em;
-    font-weight: 300;
-    margin-bottom: 32px;
-    line-height: 1.5em;
-  }
-  a {
-    font-size: 1.25em;
-    padding-bottom: 4px;
-    border-bottom: 2px solid white;
-
-    &:hover {
-      border-bottom: 2px solid $color-secondary;
-    }
-    &:active {
-      color: $color-secondary;
-    }
-  }
 }
 section.content-area {
   margin: 32px 0;
