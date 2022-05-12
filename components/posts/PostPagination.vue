838a88678ba7a8adcb7a3d0aba38d371a57d840b
김성찬 <kimsc0714@naver.com>
2022-05-12 23:50:22 +0900

    feat: 10페이지 단위로 이동하는 기능 구현

--- components/posts/PostPagination.vue
+++ components/posts/PostPagination.vue
@@ -3,10 +3,15 @@
     <div class="pagination-center">
       <IconLink :disabled="!hasPrevPage" @click="moveToFirst">
         <SvgBase icon>
+          <IconLastLeft></IconLastLeft>
+        </SvgBase>
+      </IconLink>
+      <IconLink :disabled="!hasPrevPages" @click="moveToPrevPages">
+        <SvgBase icon>
           <IconDoubleLeft></IconDoubleLeft>
         </SvgBase>
       </IconLink>
-      <IconLink :disabled="!hasPrevPage" @click="moveToPrev">
+      <IconLink :disabled="!hasPrevPage" @click="moveToPrevPage">
         <SvgBase icon>
           <IconLeft></IconLeft>
         </SvgBase>
@@ -14,23 +19,28 @@
 
       <div class="pages">
         <IconLink
-          v-for="(_, index) in new Array(totalPages)"
-          :key="index"
-          :class="{ 'curr-page': isCurrPage(index + 1) }"
-          @click="moveToPage(index + 1)"
+          v-for="pageNum in getPageRange()"
+          :key="pageNum"
+          :class="{ 'curr-page': isCurrPage(pageNum) }"
+          @click="moveToPage(pageNum)"
         >
-          {{ index + 1 }}
+          {{ pageNum }}
         </IconLink>
       </div>
 
-      <IconLink :disabled="!hasNextPage" @click="moveToNext">
+      <IconLink :disabled="!hasNextPage" @click="moveToNextPage">
         <SvgBase icon>
           <IconRight></IconRight>
         </SvgBase>
       </IconLink>
-      <IconLink :disabled="!hasNextPage" @click="moveToLast">
+      <IconLink :disabled="!hasNextPages" @click="moveToNextPages">
         <SvgBase icon>
           <IconDoubleRight></IconDoubleRight>
+        </SvgBase>
+      </IconLink>
+      <IconLink :disabled="!hasNextPage" @click="moveToLast">
+        <SvgBase icon>
+          <IconLastRight></IconLastRight>
         </SvgBase>
       </IconLink>
     </div>
@@ -52,55 +62,57 @@ export default {
   data() {
     return {
       page: 1,
+      pageUnit: 10,
     }
   },
   computed: {
     totalPages() {
       return Math.ceil(this.total / this.perPage)
     },
-    currPage() {
-      return parseInt(this.$route.params.page) || 1
-    },
     prevPage() {
-      return this.currPage > 1 ? this.currPage - 1 : 1
+      return this.page > 1 ? this.page - 1 : 1
     },
     nextPage() {
-      return this.currPage < this.totalPages
-        ? this.currPage + 1
-        : this.totalPages
+      return this.page < this.totalPages ? this.page + 1 : this.totalPages
     },
     hasPrevPage() {
-      return this.currPage !== 1
+      return this.page !== 1
     },
     hasNextPage() {
-      return this.currPage !== this.totalPages
+      return this.page !== this.totalPages
+    },
+    hasPrevPages() {
+      return Math.floor((this.page - 1) / this.pageUnit) > 0
+    },
+    hasNextPages() {
+      return (
+        Math.floor((this.page - 1) / this.pageUnit) <
+        Math.floor(this.totalPages / this.pageUnit)
+      )
     },
   },
   mounted() {
-    this.page = this.currPage
+    this.page = parseInt(this.$route.params.page) || 1
   },
   methods: {
-    onKeydown({ target, key }) {
-      if (key !== 'Enter') return
-      if (target.value > this.totalPages || target.value < 1) {
-        this.page = this.currPage
-      } else if (target.value !== this.currPage) {
-        this.$router.push(`/posts/page/${target.value}`)
-      }
-    },
-    onBlur({ target }) {
-      if (target.value > this.totalPages || target.value < 1) {
-        this.page = this.currPage
-      }
-    },
     moveToFirst() {
       if (this.hasPrevPage) this.$router.push('/posts/page/1')
     },
-    moveToPrev() {
+    moveToPrevPage() {
       if (this.hasPrevPage) this.$router.push(`/posts/page/${this.prevPage}`)
     },
-    moveToNext() {
+    moveToPrevPages() {
+      const prevLastPage =
+        Math.floor((this.page - 1) / this.pageUnit) * this.pageUnit
+      this.$router.push(`/posts/page/${prevLastPage}`)
+    },
+    moveToNextPage() {
       if (this.hasNextPage) this.$router.push(`/posts/page/${this.nextPage}`)
+    },
+    moveToNextPages() {
+      const nextFirstPage =
+        Math.floor((this.page - 1) / this.pageUnit + 1) * this.pageUnit + 1
+      this.$router.push(`/posts/page/${nextFirstPage}`)
     },
     moveToLast() {
       if (this.hasNextPage) this.$router.push(`/posts/page/${this.totalPages}`)
@@ -109,7 +121,20 @@ export default {
       this.$router.push(`/posts/page/${page}`)
     },
     isCurrPage(page) {
-      return this.currPage === page
+      return this.page === page
+    },
+    getPageRange() {
+      // 화면에 보여지는 페이지 숫자들을 배열로 리턴
+      const lastPrevPage =
+        Math.floor((this.page - 1) / this.pageUnit) * this.pageUnit
+      const visiblePageNums =
+        this.totalPages - lastPrevPage > this.pageUnit
+          ? this.pageUnit
+          : this.totalPages - lastPrevPage
+
+      return new Array(visiblePageNums)
+        .fill(0)
+        .map((_, index) => lastPrevPage + index + 1)
     },
   },
 }
