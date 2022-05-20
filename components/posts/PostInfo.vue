cfea7dd5e2db08c830a266a20aa82d96f3f9b6b1
김성찬 <kimsc0714@naver.com>
2022-05-20 23:41:00 +0900

    refactor: 포스트 상단 정보 영역을 컴포넌트로 분리

---
+++ components/posts/PostInfo.vue
@@ -0,0 +1,177 @@
+<template>
+  <section class="post-info">
+    <div class="info-main">
+      <h1>{{ post.title }}</h1>
+      <p>{{ post.description }}</p>
+    </div>
+    <div class="info-sub">
+      <div>
+        <div v-for="tag in post.tags.split(', ')" :key="tag" class="tags">
+          <span class="tag">#{{ tag }}</span>
+        </div>
+      </div>
+      <div class="posted-date">
+        <span class="created-date">{{ formatDate(post.createdAt) }}</span>
+        <span
+          v-if="diffDate(post.createdAt, post.updatedAt) > 0"
+          class="updated-date"
+        >
+          (수정: {{ formatDate(post.updatedAt) }})
+        </span>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+import { formatDate } from '@/utils/handleDate'
+
+export default {
+  props: {
+    post: {
+      type: Object,
+      required: true,
+    },
+  },
+  methods: {
+    formatDate,
+    diffDate(createdAtString, updatedAtString) {
+      const createdAt = new Date(createdAtString)
+      const updatedAt = new Date(updatedAtString)
+      const createdDate = new Date(
+        createdAt.getYear(),
+        createdAt.getMonth(),
+        createdAt.getDate(),
+      )
+      const updatedDate = new Date(
+        updatedAt.getYear(),
+        updatedAt.getMonth(),
+        updatedAt.getDate(),
+      )
+      return updatedDate - createdDate
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+$background-height: calc(#{$header-image-height} - #{$header-height});
+
+.post-info {
+  height: $background-height;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-end;
+  color: white;
+  padding: 32px;
+
+  .info-main {
+    p {
+      margin-top: 8px;
+    }
+  }
+  .info-sub {
+    display: flex;
+    justify-content: space-between;
+    color: rgba(white, 0.5);
+    margin: 8px 0;
+  }
+}
+.tags {
+  display: inline;
+  margin-right: 8px;
+
+  &:last-child {
+    margin-right: 0;
+  }
+}
+
+@include viewpoint-xl {
+  .post-info {
+    width: $breakpoint-xl;
+  }
+  .info-main {
+    h1 {
+      font-size: 2.5em;
+    }
+    p {
+      font-size: 1.5em;
+    }
+  }
+  .info-sub {
+    font-size: 1em;
+  }
+}
+@include viewpoint-lg {
+  .post-info {
+    width: $breakpoint-lg;
+  }
+  .info-main {
+    h1 {
+      font-size: 2.25em;
+    }
+    p {
+      font-size: 1.25em;
+    }
+  }
+  .info-sub {
+    font-size: 1em;
+  }
+}
+@include viewpoint-md {
+  .post-info {
+    width: $breakpoint-md;
+  }
+  .info-main {
+    h1 {
+      font-size: 2em;
+    }
+    p {
+      font-size: 1.25em;
+    }
+  }
+  .info-sub {
+    font-size: 0.9em;
+  }
+}
+@include viewpoint-sm {
+  .post-info {
+    width: $breakpoint-sm;
+  }
+  .info-main {
+    h1 {
+      font-size: 1.75em;
+    }
+    p {
+      font-size: 1.1em;
+    }
+  }
+  .info-sub {
+    font-size: 0.8em;
+  }
+}
+@include viewpoint-xs {
+  .post-info {
+    width: 100vw;
+
+    .info-sub {
+      display: block;
+    }
+  }
+  .info-main {
+    h1 {
+      font-size: 1.75em;
+    }
+    p {
+      font-size: 1.1em;
+    }
+  }
+  .info-sub {
+    font-size: 0.8em;
+
+    .posted-date {
+      float: right;
+    }
+  }
+}
+</style>
--- pages/posts/_slug.vue
+++ pages/posts/_slug.vue
@@ -1,27 +1,6 @@
 <template>
   <article>
-    <section class="post-info">
-      <div class="info-main">
-        <h1>{{ post.title }}</h1>
-        <p>{{ post.description }}</p>
-      </div>
-      <div class="info-sub">
-        <div>
-          <div v-for="tag in post.tags.split(', ')" :key="tag" class="tags">
-            <span class="tag">#{{ tag }}</span>
-          </div>
-        </div>
-        <div class="posted-date">
-          <span class="created-date">{{ formatDate(post.createdAt) }}</span>
-          <span
-            v-if="diffDate(post.createdAt, post.updatedAt) > 0"
-            class="updated-date"
-          >
-            (수정: {{ formatDate(post.updatedAt) }})
-          </span>
-        </div>
-      </div>
-    </section>
+    <PostInfo :post="post"></PostInfo>
     <post-content-container :post="post"></post-content-container>
     <post-prev-next :prev="prev" :next="next" />
     <post-comment />
@@ -30,7 +9,6 @@
 
 <script>
 import { getPattern } from '@/utils/pattern'
-import { formatDate } from '@/utils/handleDate'
 
 export default {
   async asyncData({ $content, params, error }) {
@@ -57,26 +35,9 @@ export default {
   },
   created() {
     this.$store.commit('SET_HEADER_IMAGE', getPattern(this.post.title))
-    console.log(this.post)
   },
   methods: {
     getPattern,
-    formatDate,
-    diffDate(createdAtString, updatedAtString) {
-      const createdAt = new Date(createdAtString)
-      const updatedAt = new Date(updatedAtString)
-      const createdDate = new Date(
-        createdAt.getYear(),
-        createdAt.getMonth(),
-        createdAt.getDate(),
-      )
-      const updatedDate = new Date(
-        updatedAt.getYear(),
-        updatedAt.getMonth(),
-        updatedAt.getDate(),
-      )
-      return updatedDate - createdDate
-    },
   },
   head() {
     return {
@@ -85,128 +46,3 @@ export default {
   },
 }
 </script>
-
-<style lang="scss">
-$background-height: calc(#{$header-image-height} - #{$header-height});
-
-article {
-  margin-bottom: 32px;
-}
-.post-info {
-  height: $background-height;
-  display: flex;
-  flex-direction: column;
-  justify-content: flex-end;
-  color: white;
-  padding: 32px;
-
-  .info-main {
-    p {
-      margin-top: 8px;
-    }
-  }
-  .info-sub {
-    display: flex;
-    justify-content: space-between;
-    color: rgba(white, 0.5);
-    margin: 8px 0;
-  }
-}
-.tags {
-  display: inline;
-  margin-right: 8px;
-
-  &:last-child {
-    margin-right: 0;
-  }
-}
-
-@include viewpoint-xl {
-  .post-info {
-    width: $breakpoint-xl;
-  }
-  .info-main {
-    h1 {
-      font-size: 2.5em;
-    }
-    p {
-      font-size: 1.5em;
-    }
-  }
-  .info-sub {
-    font-size: 1em;
-  }
-}
-@include viewpoint-lg {
-  .post-info {
-    width: $breakpoint-lg;
-  }
-  .info-main {
-    h1 {
-      font-size: 2.25em;
-    }
-    p {
-      font-size: 1.25em;
-    }
-  }
-  .info-sub {
-    font-size: 1em;
-  }
-}
-@include viewpoint-md {
-  .post-info {
-    width: $breakpoint-md;
-  }
-  .info-main {
-    h1 {
-      font-size: 2em;
-    }
-    p {
-      font-size: 1.25em;
-    }
-  }
-  .info-sub {
-    font-size: 0.9em;
-  }
-}
-@include viewpoint-sm {
-  .post-info {
-    width: $breakpoint-sm;
-  }
-  .info-main {
-    h1 {
-      font-size: 1.75em;
-    }
-    p {
-      font-size: 1.1em;
-    }
-  }
-  .info-sub {
-    font-size: 0.8em;
-  }
-}
-@include viewpoint-xs {
-  .post-info {
-    width: 100vw;
-
-    .info-sub {
-      display: block;
-    }
-  }
-  .info-main {
-    h1 {
-      font-size: 1.75em;
-    }
-    p {
-      font-size: 1.1em;
-    }
-  }
-  .info-sub {
-    font-size: 0.8em;
-
-    .posted-date {
-      float: right;
-    }
-  }
-}
-</style>
