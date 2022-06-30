792e0fc7139e70aa737d6a95b934eef411057631
김성찬 <kimsc0714@naver.com>
2022-06-30 23:54:28 +0900

    feat(style): 폰트 크기, 굵기, 간격 수정

--- components/about/AboutContacts.vue
+++ components/about/AboutContacts.vue
@@ -1,16 +1,9 @@
 <template>
-  <section class="about-contacts">
-    <h1>Contacts (연락처 및 링크)</h1>
-    ### E-mail [kimsc0714@gmail.com](mailto://kimsc0714@gmail.com) ### GitHub
-    [https://github.com/sckroll](https://github.com/sckroll)
+  <section class="about">
+    <div class="section-content">
+      <h2>연락처 및 링크</h2>
+      <p>E-mail [kimsc0714@gmail.com](mailto://kimsc0714@gmail.com)</p>
+      <p>GitHub [https://github.com/sckroll](https://github.com/sckroll)</p>
+    </div>
   </section>
 </template>
-
-<script>
-export default {}
-</script>
-
-<style lang="scss" scoped>
-section {
-}
-</style>
--- pages/about/index.vue
+++ pages/about/index.vue
@@ -18,3 +18,25 @@ export default {
   },
 }
 </script>
+
+<style lang="scss">
+section.about {
+  h1 {
+    font-size: 2em;
+    font-weight: 500;
+  }
+  h2 {
+    font-size: 1.5em;
+    font-weight: 500;
+  }
+  p {
+    font-size: 1.25em;
+    line-height: 1.5em;
+  }
+}
+.section-content {
+  display: flex;
+  flex-direction: column;
+  gap: 24px;
+}
+</style>
