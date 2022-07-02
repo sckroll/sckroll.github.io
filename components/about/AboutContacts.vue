b7f15ad589c47053a58674248c04ae388f1824ed
김성찬 <kimsc0714@naver.com>
2022-07-02 23:31:44 +0900

    feat(style): 연락처 섹션 디자인 변경

--- components/about/AboutContacts.vue
+++ components/about/AboutContacts.vue
@@ -1,7 +1,36 @@
 <template>
   <section class="about-content">
     <h1>연락처 및 링크</h1>
-    <p>E-mail [kimsc0714@gmail.com](mailto://kimsc0714@gmail.com)</p>
-    <p>GitHub [https://github.com/sckroll](https://github.com/sckroll)</p>
+    <div class="contacts">
+      <div class="contact">
+        <div class="contact-label">E-mail</div>
+        <TextLink to="mailto://kimsc0714@gmail.com" anchor>
+          kimsc0714@gmail.com
+        </TextLink>
+      </div>
+      <div class="contact">
+        <div class="contact-label">GitHub</div>
+        <TextLink to="https://github.com/sckroll" anchor>
+          https://github.com/sckroll
+        </TextLink>
+      </div>
+    </div>
   </section>
 </template>
+
+<style lang="scss" scoped>
+.contacts {
+  display: flex;
+  flex-direction: column;
+}
+.contact {
+  display: flex;
+  align-items: center;
+  font-size: 1.25em;
+  gap: 24px;
+}
+.contact-label {
+  font-weight: 700;
+  margin-bottom: 2px;
+}
+</style>
