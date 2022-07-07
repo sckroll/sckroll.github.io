7a7d8717985645865efdac9751d39462877b3e84
김성찬 <kimsc0714@naver.com>
2022-07-07 21:52:46 +0900

    feat: asyncData 대신 fetch로 포스트 데이터를 불러오도록 수정

--- pages/posts/search.vue
+++ pages/posts/search.vue
@@ -14,123 +14,39 @@
 import { skipPost, getLastPage } from '@/utils/postUtils'
 
 export default {
-  async asyncData({ $content, query, error, redirect, store }) {
+  async fetch() {
     try {
       // 검색 초기 페이지일 경우
-      if (!query.page && !query.q && !query.field)
+      if (!this.page && !this.query && !this.field)
         return { posts: [], totalPostCount: 0 }
 
       // 쿼리스트링이 하나라도 없거나 올바른 형식이 아니면 초기 페이지로 리다이렉트
-      if (!query.page || isNaN(query.page) || !query.q || !query.field)
-        redirect('/posts/search')
+      if (!this.page || isNaN(this.page) || !this.query || !this.field) {
+        this.$router.replace({ path: '/posts/search', query: {} })
+        return
+      }
 
       // 한 페이지당 포스트 개수
-      const perPage = store.state.perPage
+      this.perPage = this.$store.state.perPage
 
       // 검색된 총 포스트 개수
-      const allPosts = await $content('posts', { deep: true })
+      const allPosts = await this.$content('posts', { deep: true })
         .only([])
-        .where({ [query.field]: { $contains: query.q } })
+        .where({ [this.field]: { $contains: this.query } })
         .fetch()
-      const totalPostCount = allPosts.length
+      this.totalPostCount = allPosts.length
 
       // 페이지 범위 밖에 있으면 1페이지로 리다이렉트
-      const lastPage = getLastPage(totalPostCount)
-      if (query.page < 1 || (lastPage > 0 && query.page > lastPage))
-        redirect(`/posts/search?q=${query.q}&field=${query.field}&page=1`)
+      const lastPage = getLastPage(this.totalPostCount)
+      if (this.page < 1 || (lastPage > 0 && this.page > lastPage))
+        this.$router.replace(
+          `/posts/search?q=${this.query}&field=${this.field}&page=1`,
+        )
 
       // search()를 사용하면 한 글자만 달라도 검색 결과에 포함되므로
       // (ex: tree를 검색했을 때 trie도 결과에 포함됨)
       // where() 안에 MongoDB 문법인 $contains를 사용하여
       // 검색어를 오타 없이 완전히 포함한 모든 결과를 쿼리
-      const posts = await $content('posts', {
-        deep: true,
-        text: true,
-      })
-        .only([
-          'title',
-          'description',
-          'img',
-          'slug',
-          'tags',
-          'createdAt',
-          'updatedAt',
-          'text',
-        ])
-        .where({ [query.field]: { $contains: query.q } })
-        .sortBy('createdAt', 'desc')
-        .limit(perPage)
-        .skip(skipPost(query.page, totalPostCount))
-        .fetch()
-
-      return {
-        posts,
-        totalPostCount,
-        perPage,
-      }
-    } catch (e) {
-      error({ statusCode: e.statusCode || e.status || 500 })
-    }
-  },
-  computed: {
-    query() {
-      return this.$route.query.q || ''
-    },
-    field() {
-      return this.$route.query.field || ''
-    },
-    page() {
-      return this.$route.query.page || ''
-    },
-  },
-  watch: {
-    async query(val) {
-      try {
-        this.$nuxt.$loading.start()
-
-        // 검색 초기 페이지로 돌아갈 경우
-        if (!val) {
-          this.posts = null
-          this.totalPostCount = 0
-          return
-        }
-
-        // perPage 값이 없을 경우 초기화
-        if (!this.perPage) this.perPage = this.$store.state.perPage
-
-        // 검색된 총 포스트 개수
-        const allPosts = await this.$content('posts', { deep: true })
-          .only([])
-          .where({ [this.field]: { $contains: val } })
-          .fetch()
-        this.totalPostCount = allPosts.length
-
-        // 현재 페이지의 포스트를 배열에 저장
-        await this.updatePosts()
-
-        this.$nuxt.$loading.finish()
-      } catch (e) {
-        this.$nuxt.$loading.finish()
-        this.$nuxt.error({ statusCode: e.statusCode || e.status || 500 })
-      }
-    },
-    async page() {
-      try {
-        this.$nuxt.$loading.start()
-
-        // 현재 페이지의 포스트를 배열에 저장
-        await this.updatePosts()
-
-        this.$nuxt.$loading.finish()
-        window.scrollTo(0, 0)
-      } catch (e) {
-        this.$nuxt.$loading.finish()
-        this.$nuxt.error({ statusCode: e.statusCode || e.status || 500 })
-      }
-    },
-  },
-  methods: {
-    async updatePosts() {
       this.posts = await this.$content('posts', {
         deep: true,
         text: true,
@@ -150,6 +66,32 @@ export default {
         .limit(this.perPage)
         .skip(skipPost(this.page, this.totalPostCount))
         .fetch()
+    } catch (e) {
+      this.$nuxt.error({ statusCode: e.statusCode || e.status || 500 })
+    }
+  },
+  data() {
+    return {
+      posts: [],
+      totalPostCount: 0,
+      perPage: 0,
+    }
+  },
+  computed: {
+    query() {
+      return this.$route.query.q || ''
+    },
+    field() {
+      return this.$route.query.field || ''
+    },
+    page() {
+      return this.$route.query.page || ''
+    },
+  },
+  watch: {
+    '$route.query': '$fetch',
+    page() {
+      window.scrollTo(0, 0)
     },
   },
   head() {
