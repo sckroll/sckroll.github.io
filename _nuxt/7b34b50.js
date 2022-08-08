a1638a1765d119d2b65063d814da7da28ba3618a
sckroll <sckroll@users.noreply.github.com>
2022-08-08 14:52:41 +0000

    deploy: d57910c753df784dadeaa79b20e19f30a66cb3a3

---
+++ _nuxt/7b34b50.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[21],{351:function(t,n,e){"use strict";e.r(n);var l=e(2),component=Object(l.a)({},(function(){var t=this.$createElement;return(this._self._c||t)("path",{attrs:{d:"M24 16L10.3636 30L9 28.6L15.1364 22.3L21.2727 16L9 3.4L10.3636 2L24 16Z"}})}),[],!1,null,null,null);n.default=component.exports}}]);
