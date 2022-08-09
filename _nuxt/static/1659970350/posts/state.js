2c03480a876f1ee8d38aae722dbe79ce8345f46c
sckroll <sckroll@users.noreply.github.com>
2022-08-09 13:49:50 +0000

    deploy: b041c984c0114843f8b0f8ad7739feaa7e13dbda

--- _nuxt/static/1659970350/posts/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1659970350",layout:"default",error:a,state:a,serverRendered:false,routePath:"\u002Fposts",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"5bc4f74f"}}}}(null));
--- _nuxt/static/1659970350/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1659970350",layout:"default",error:a,state:a,serverRendered:false,routePath:b,config:{_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"5bc4f74f"}}}}(null,"\u002F"));
