c428ae315f6c49b051c615050dd8d9738a001e30
sckroll <sckroll@users.noreply.github.com>
2022-08-11 13:42:57 +0000

    deploy: 3b96ac8b350de7c891097004c5e1db95e65b7be7

---
+++ _nuxt/static/1660225368/posts/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1660225368",layout:"default",error:a,state:a,serverRendered:false,routePath:"\u002Fposts",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"ce871ba0"}}}}(null));
