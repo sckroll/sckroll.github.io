0931798e94d216af3e8d936a22a31a65cbb9eba1
Sckroll <kimsc0714@gmail.com>
2023-03-15 00:04:03 +0900

    🧹 chore(assets): 기존 브랜치로부터 에셋 복사 및 전역 스타일링 적용

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,15 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-
+  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: `
+            @use "@/assets/scss/_variables.scss" as *;
+            @use "@/assets/scss/_mixin.scss" as *;
+          `
+        }
+      }
+    }
+  }
 })
